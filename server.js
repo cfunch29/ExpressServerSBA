5161c1e2fb9aece09f1120deba8d6fd2dc6023a8
cfunch29 <cfunch29@gmail.com>
2026-01-23 17:25:31 -0500

    added listen and global err handling middleware

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ const PORT = 3000;
 // Routes 
 
 // Global Err Handling Middleware 
+app.use("/", (err, req, res, next) => {
+    console.log(err.message)
+    res.status(err.status || 500).json({error: err.message});
+})
 
 // Listen 
-
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+})
