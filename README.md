0a2f692f5f20029e4b96355f05a1e7cdfdb3fdd4
cfunch29 <cfunch29@gmail.com>
2026-01-28 00:12:02 -0500

    updated index.html and views. added data to README

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
 # Express Server 
+I wanted to create a contact message log. 
 
 
+## Technologies Used 
+- HTML
+- CSS
+- JavaScript
+- Node.js
+- Express
+- Postman
 
-## 
+## Reflections
+What could you have done differently during the planning stages of your project to make the execution easier?
+- I should have planned out the different databases better. I was confused with how we were supposed to set it up. I thought we needed 3 properties for
+each database but we needed 3 categories. Although we didn't need to handle routes for each page I still got lost trying to implement each page. 
+- I was also able to get my routes to post using postman but for some reason couldn't connect my server to my frontend to post the data. 
 
-##
+Were there any requirements that were difficult to implement? What do you think would make them easier to implement in future projects?
+- The view/view engine was difficult. Also setting up the routes for each category and making sure each one would manipulate data from each path THEN show on the frontend. 
+
+What would you add to or change about your application if given more time?
+- I would make it more aesthetically pleasing but I would also make sure all functions were working. I should have added more error handling but that also confuses me. 
 
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import express from 'express';
 import contactRoutes from "./routes/contactRoutes.js"
 import postRoutes from "./routes/postRoutes.js"
 import db from "./database/comments.js"
+import fs from 'fs';
 
 
 //import data from database files 
@@ -16,18 +17,32 @@ import db from "./database/comments.js"
 import { globalErr } from './middleware/middlewares.js';
 import logReq from './middleware/logReq.js';
 
-// Data 
 
 // setups 
 const app = express(); //instantiate express into variable 
 const PORT = 3000;
 
 // view engine 
-
+app.engine("html", function (filePath, options, cb){
+    fs.readFile(filePath, (err, content) => {
+        if (err) return cb(err);
+        
+         let rendered = content.toString();
+        if (options && options.list){
+            rendered = rendered.replace("#list#", options.list.join(""));
+        }
+        return cb(null, rendered);
+        }
+    )
+});
 
 // view engine setup 
+app.set("views", "./views");
+app.set("view engine", "html");
+app.use(express.static('./styles'));
 
 // Middleware 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); //Parses the req body so we can use 
 //request log time and req method
 app.use(logReq);
@@ -36,9 +51,10 @@ app.use(logReq);
 
 
 // Routes 
-
+// render view to index
 app.get("/", (req, res) => {
-    res.send("Home")
+    const list = db.map(post => `<li>${post.message}</li>`);
+    res.render("index", { list })
 });
 
 app.use("/api/contacts", contactRoutes);
